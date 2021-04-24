dbb5c25912b5af3add3437979b00ddf17b976bf2
rino.tajima <rino.taji111@gmail.com>
2021-04-24 11:38:46 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :tasks
     root to: 'tasks#index'
-    
+  
+  #ログイン機能のルート 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+   
+   #ユーザ機能のルート 
     get 'signup', to: 'users#new' 
     resources :users, only: [:index, :show, :create]
 end
